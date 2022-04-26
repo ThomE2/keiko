ca063a1b5eb408c66cdd0e1a27d8bbd682bb978a
Thomas Eudes <thomas.eudes@yahoo.com>
2022-04-26 10:56:47 +0200

    added Bulbasaur to home page

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -2,9 +2,16 @@ import styles from "./Home.module.css"
 
 export const Home = () => {
   return (
-    <div className={styles.intro}>
-      <div>Bienvenue sur ton futur pokédex !</div>
-      <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React et attraper des pokemons !</div>
+    <div>
+      <div className={styles.intro}>
+        <div>Bienvenue sur ton futur pokédex !</div>
+        <div>Tu vas pouvoir apprendre tout ce qu'il faut sur React et attraper des pokemons !</div>
+      </div>
+      <div>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"></img>
+        <p>Nom : Bulbizarre</p>
+        <p>Numéro : 001</p>
+      </div>
     </div>
   )
 }
